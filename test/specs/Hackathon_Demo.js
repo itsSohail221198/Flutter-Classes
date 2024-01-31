49bd0d14ed672b96d69dba757e34cf65c75b3424
Sohail Ansari <sohail.ansari@expeditors.com>
2024-01-31 13:46:52 +0530

    Changes in the Demo project

--- test/specs/Hackathon_Demo.js
+++ test/specs/Hackathon_Demo.js
@@ -4,29 +4,53 @@ const flutterDriver = require('appium-flutter-driver')
 const allureReporter = require('@wdio/allure-reporter')
 
 describe('Launch App and Order a product', () => {
+
+    const getStartedBtn = find.byText('Get Started')
+    const oneProduct = find.byText('Second shirt');
+    const sSize = find.byText('S')
+    const mSize = find.byText('M')
+    const xlSize = find.byText('XL')
+    const orderNow = find.byValueKey("Order Now Button")
+
     it('Click button and select the product', async () => {
-        const getStartedBtn = find.byText('Get Started')
-        const oneProduct = find.byText('Second shirt');
-        const sSize = find.byText('S')
-        const mSize = find.byText('M')
-        const xlSize = find.byText('XL')
-        const orderNow = find.byValueKey("Order Now Button")
-        allureReporter.addFeature('Shopping');
+        allureReporter.addFeature('Shopping')
         
         await driver.execute('flutter:waitFor', getStartedBtn)
         assert.strictEqual(await driver.getElementText(getStartedBtn), 'Get Started')
         await driver.elementClick(getStartedBtn)
+        
                     
         await driver.elementClick(oneProduct)
-
+        
         await driver.elementClick(sSize)
+        
+        await driver.elementClick(mSize)
+        
+        await driver.elementClick(xlSize)
 
         await driver.elementClick(mSize)
+
+        await driver.elementClick(sSize)
 
         await driver.elementClick(xlSize)
     
         await driver.elementClick(orderNow)
 
+        await driver.elementClick(orderNow)
+
+        await driver.elementClick(orderNow)
+        
     })
+
+it('Negative Scenario for testing', async() =>{
+ 
+    await driver.elementClick(orderNow)
+    assert.strictEqual(await driver.getElementText(orderNow), 'Get Started')
+
+})
+
+    
+
+
 })
 
